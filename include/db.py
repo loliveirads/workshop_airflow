fb87431fef1f9e71adc8c2550d3340092a6ca37c
Luiz Fernando <luizfsoliveira.lm@gmail.com>
2025-01-16 18:55:22 -0300

    arrumando o db.py

--- include/db.py
+++ include/db.py
@@ -4,13 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-
-load_dotenv(dotenv_path=".env")
-
 # Carregar arquivo .env
 if not load_dotenv(dotenv_path=".env"):
     raise FileNotFoundError("Arquivo .env não encontrado ou não pôde ser carregado")
 
+# Variáveis de conexão
 db_user = os.getenv("POSTGRES_USER")
 db_password = os.getenv("POSTGRES_PASSWORD")
 db_name = os.getenv("POSTGRES_DB")
@@ -19,14 +17,19 @@ db_port = os.getenv("DB_PORT")
 
 if not db_port or not db_port.isdigit():
     raise ValueError(f"DB_PORT não foi carregado corretamente: {db_port}")
-print(f"DB_PORT carregado com sucesso: {db_port}")
-
 db_port = int(db_port)  # Converte para inteiro
 
 # Conexão dinâmica com o banco de dados
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 engine = create_engine(DATABASE_URL)
 
-
+# Sessão e Base
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Testando a conexão com o banco
+try:
+    with engine.connect() as connection:
+        print("Conexão com o banco de dados foi bem-sucedida!")
+except Exception as e:
+    print(f"Erro ao conectar ao banco de dados: {e}")
